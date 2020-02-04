404b1e49d9cc88220ac05094317acf75a8012b94
teun <teun.vanhelvoort2@edu-kw1c.nl>
2020-02-04 12:14:03 +0100

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "Index"; ?></title>
+    <link href="index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <div>
+        <h1>Dit zijn mijn Opdrachten:</h1>
+        <ul>
+            <li>
+                <a href="helloworld.php">helloworld.php</a>
+            </li>
+        </ul>
+    </div>
+
+
+</body>
+</html>
