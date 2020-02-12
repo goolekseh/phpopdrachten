abd3ae84555cf15b6e34a24b656d3107454230c8
teun <teun.vanhelvoort2@edu-kw1c.nl>
2020-02-12 16:03:34 +0100

    Uploaden opdracht 2.1 en 2.2.

---
+++ Opdrachten/hoofdstuk3/opdracht 3.1.php
@@ -0,0 +1,34 @@
+<!-- /**
+* User: Teun van Helvoort
+* Date: 5/2/2020
+* Time: 2:53 PM
+* File: opdracht 2.1.php
+-->
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "opdracht 2.1.php"; ?></title>
+    <link href="../template%20css/template.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div id="header">
+    <h1><a href="../../index.php">Inlever Opdrachten PHP</a></h1>
+</div>
+<?php
+    $evenement = "Elfstedentocht";
+    $fries = "Alvestedetocht";
+    $kilometers = 200;
+    $actie = "schaatstocht";
+    $ijs = "natuurijs";
+    $vereniging = "Koninklijke Vereniging De Friesche Elf Steden.";
+    $plaatsnaam = "Leeuwarden";
+    $provincie = "Friesland";
+    $jaar = 1909;
+    $aantal = 1;
+
+
+?>
+</body>
+</html>
