0082a52a4574c46c9b55a1bbe68deef033dbf094
teun <teun.vanhelvoort2@edu-kw1c.nl>
2020-05-30 18:57:06 +0200

    7.4 afgerond en getest.

--- Opdrachten/hoofdstuk 7/insert.php
+++ Opdrachten/hoofdstuk 7/insert.php
@@ -4,9 +4,11 @@
 
     <label>Joketext          <input placeholder="Joketext komt hier" name="textResult"></label><br>
     <label>Jokeclou     <input placeholder="Jokeclou komt hier" name="clouResult"></label><br>
-    <input type="submit">
+    <input type="submit" name="submit">
 </form>
 <?php
+include "functions.php";
+startConnection();
 try {
 $pdo = new PDO("odbc:odbc2sqlserver");
 }
@@ -20,20 +22,28 @@ die();
 echo "database connectie gelukt <br><br>";
 ?>
 <?php
-include "functions.php";
+
 
 $query = "";
-if(isset($_POST["textResult"]) && isset($_POST["clouResult"]))
-{
-    $query = "INSERT INTO dbo.joke (joketext, jokeclou, jokedate) VALUES ('','', GETDATE())";
-    if (!empty($_POST["textResult"])) {
-        $src = $_POST["textResult"];
-        $src2 = $_POST["clouResult"];
 
-        $query = "INSERT INTO dbo.joke VALUES ('".$src."','".$src2."', GETDATE())";
-    }
+if(isset($_POST["submit"]))
+{
+    $query = "INSERT INTO dbo.joke VALUES ('". $_POST["textResult"] ."', '" . $_POST["clouResult"] . "', GETDATE())";;
+    // echo $query;
+    executeQueryViaExec($query);
+    echo
+        "<h1>" . "Grap toegevoegd" . "</h1>" .
+        "<p>" . "Bedankt voor het toevoegen van je grap. Hieronder zie je een overzicht van je grap:" . "</p>" .
+        "<p>" . "Joketext: " . $_POST["textResult"] . "</p>" .
+        "<p>" . "Jokeclou: " . $_POST["textResult"] . "</p>" .
+
+        "<a href='opdracht73.php'>" . "Bekijk grappen (opdracht 7.3)" . "</a>";
 }
-echo $query;
+
+
+
+
+
 
 
 
